d6593ce08226b533c1cd6a04eab5f855986943a7
Laurence Muller <laurence.muller@gmail.com>
2011-06-02 15:28:39 +0200

    Initial import source code fIRC 3

---
+++ fIRC3/src/com/falcon4ever/fIRC3/HomeActivity.java
@@ -0,0 +1,34 @@
+/* 
+ * fIRC - a free IRC client for the Android platform.
+ * http://code.google.com/p/firc-chat/
+ * 
+ * Copyright (C) 2008-2011 Laurence Muller <laurence.muller@gmail.com>
+ * http://www.multigesture.net/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package com.falcon4ever.fIRC3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
